787ac310d27f7792ad412e8e638dc1ed086ac42f
Ana Asuncion <ana.asuncion@9dots.org>
2019-12-26 11:57:46 -0800

    refactor Todos component with useState hook

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,214 +2,113 @@ import React from 'react'
 import AddTodo from './AddTodo'
 import TodoList from './TodoList'
 import ListMenu from './ListMenu'
-import { Tooltip, Icon } from 'antd'
 
+const Todos = () => {
+    const [todos, setTodos] = React.useState([])
+    const [input, setInput] = React.useState('')
+    const [isAllComplete, setIsAllComplete] = React.useState(false)
+    const [filter, setFilter] = React.useState('all')
+    const [nextKey, setNextKey] = React.useState(0)
+    const [editText, setEditText] = React.useState('')
+    const [editKey, setEditKey] = React.useState(-1)
 
-export default class Todos extends React.Component {
-    constructor(props) {
-        super(props)
+    const updateInput = (e) => setInput(e.target.value)
 
-        this.state = {
-            todos: [],
-            input: '',
-            isAllComplete: false,
-            filter: 'all',
-            nextKey: 0,
-            editText: '',
-            editKey: -1,
-        }
-
-        this.updateInput = this.updateInput.bind(this)
-        this.handleAddTodo = this.handleAddTodo.bind(this)
-        this.handleDeleteTodo = this.handleDeleteTodo.bind(this)
-        this.handleChangeTodoText = this.handleChangeTodoText.bind(this)
-        this.handleToggleTodo = this.handleToggleTodo.bind(this)
-        this.handleEditItem = this.handleEditItem.bind(this)
-        this.updateEditText = this.updateEditText.bind(this)
-        this.updateFilter = this.updateFilter.bind(this)
-        this.handleClearCompleted = this.handleClearCompleted.bind(this)
-        this.handleToggleCompleteAll = this.handleToggleCompleteAll.bind(this)
-        //this.filterTodos = this.filterTodos.bind(this)
-    }
-
-    updateInput(e) {
-        const value = e.target.value;
-
-        this.setState({
-            input: value,
-        })
-    }
-
-    handleAddTodo(e) {
-        if (e.keyCode === 13) {
-            this.setState((currentState) => {
-                return {
-                    todos: currentState.todos.concat([{
-                        item: this.state.input,
-                        isActive: true,
-                        key: this.state.nextKey,
-                    }]),
-                    input: '',
-                    isAllComplete: false,
-                    nextKey: this.state.nextKey + 1,
-                }
-
-            })
+    const handleAddTodo = (e) => {
+        if (e.key === 'Enter') {
+            setTodos(todos.concat({
+                item: input,
+                isActive: true,
+                key: nextKey,
+            }))
+            setInput('')
+            setIsAllComplete(false)
+            setNextKey(nextKey + 1)
         }
     }
-
-    handleDeleteTodo(key) {
-        this.setState((currentState) => {
-            const newStateOfTodos = currentState.todos.filter((todo) => todo.key !== key)
-            const numOfActiveItems = newStateOfTodos.filter((todo) => todo.isActive === true).length
-            if (numOfActiveItems === 0) {
-                return {
-                    isAllComplete: true,
-                    todos: newStateOfTodos
-                }
-            } else {
-                return {
-                    isAllComplete: false,
-                    todos: newStateOfTodos
-                }
-            }
-        })
+    const handleDeleteTodo = (key) => {
+        setTodos(todos.filter((todo) => todo.key !== key))
+        const numOfActiveItems = todos.filter((todo) => todo.isActive === true).length
+        numOfActiveItems === 0 ? setIsAllComplete(true) : setIsAllComplete(false)
     }
 
-    handleChangeTodoText(key, e) {
-        const editText = this.state.editText
-        if (editText === '') {
-            return this.handleDeleteTodo(key)
-        }
-        this.setState((currentState) => {
-
-            const newStateOfTodos = currentState.todos.map((todo) => {
-                if (todo.key === key) {
-                    return {
-                        ...todo, item: editText,
-                    }
-                } else {
-                    return todo
-                }
-            })
-
-            return {
-                todos: newStateOfTodos,
-                editKey: -1,
-                editText: '',
-            }
-        })
+    const handleChangeTodoText = (key) => {
+        setTodos(todos.map((todo) => {
+            if (todo.key === key)
+                return { ...todo, item: editText }
+            else
+                return todo
+        }))
+        if (!editText.trim())
+            handleDeleteTodo(key)
+        setEditKey(-1)
+        setEditText('')
     }
 
-    handleToggleTodo(key) {
-        this.setState((currentState) => {
-            const newStateOfTodos = currentState.todos.map((todo) => {
-                if (todo.key === key) {
-                    return { ...todo, isActive: !todo.isActive }
-                } else {
-                    return todo;
-                }
-            })
-            const numOfActiveItems = newStateOfTodos.filter((todo) => todo.isActive === true).length
-            if (numOfActiveItems === 0) {
-                return {
-                    todos: newStateOfTodos,
-                    isAllComplete: true
-                }
-            } else {
-                return {
-                    todos: newStateOfTodos,
-                    isAllComplete: false
-                }
-            }
-
-        })
+    const handleToggleTodo = (key) => {
+        setTodos(todos.map((todo) => {
+            if (todo.key === key)
+                return { ...todo, isActive: !todo.isActive }
+            else
+                return todo
+        }))
+        const numOfActiveItems = todos.filter((todo) => todo.isActive === true).length
+        numOfActiveItems === 0 ? setIsAllComplete(true) : setIsAllComplete(false)
     }
 
-    handleEditItem(key, item) {
-        this.setState({
-            editKey: key,
-            editText: item
-        })
+    const handleEditItem = (key, item) => {
+        setEditKey(key)
+        setEditText(item)
     }
 
-    updateEditText(e) {
-        const value = e.target.value
+    const updateEditText = (e) => setEditText(e.target.value)
 
-        this.setState({
-            editText: value
-        })
+    const updateFilter = (newFilter) => setFilter(newFilter)
+
+    const handleClearCompleted = () => {
+        setTodos(todos.filter((todo) => todo.isActive === true))
+        const numOfActiveItems = todos.length
+        numOfActiveItems === 0 ? setIsAllComplete(true) : setIsAllComplete(false)
     }
 
-    updateFilter(newFilter) {
-        this.setState({
-            filter: newFilter
-        })
+    const handleToggleCompleteAll = () => {
+        setIsAllComplete(!isAllComplete)
+        setTodos(todos.map((todo) => {
+            return { ...todo, isActive: isAllComplete }
+        }))
     }
 
-    handleClearCompleted() {
-        this.setState((currentState) => {
-            const newStateOfTodos = currentState.todos.filter((todo) => todo.isActive === true)
-            const numOfActiveItems = newStateOfTodos.length
-            if (numOfActiveItems === 0) {
-                return {
-                    isAllComplete: true,
-                    todos: newStateOfTodos
-                }
-            } else {
-                return {
-                    isAllComplete: false,
-                    todos: newStateOfTodos
-                }
-            }
-        })
-    }
+    return (
+        <React.Fragment>
+            <h1 className='center'>Todo</h1>
+            <AddTodo
+                todos={todos}
+                onToggleCompleteAll={handleToggleCompleteAll}
+                placeholder='What needs to be done?'
+                value={input}
+                onChange={updateInput}
+                onKeyDown={handleAddTodo}
+            />
+            <TodoList
+                todos={todos}
+                filter={filter}
+                editKey={editKey}
+                onDeleteTodo={handleDeleteTodo}
+                onToggleTodo={handleToggleTodo}
+                onChangeTodoText={handleChangeTodoText}
 
+                onEditItem={handleEditItem}
+                onUpdateEditText={updateEditText}
+                value={editText}
+            />
+            <ListMenu
+                todos={todos}
+                onFilterChange={updateFilter}
+                onClearCompleted={handleClearCompleted}
+            />
+        </React.Fragment>
+    )
 
-    // updates all todos to toggle active or not active
-    handleToggleCompleteAll() {
-        this.setState((currentState) => {
-            return {
-                ...currentState,
-                isAllComplete: !currentState.isAllComplete,
-                todos: currentState.todos.map((todo) => ({
-                    ...todo,
-                    isActive: currentState.isAllComplete
-                }))
-            }
-        })
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <h1 className='center'>Todo</h1>
-                <AddTodo
-                    todos={this.state.todos}
-                    onToggleCompleteAll={this.handleToggleCompleteAll}
-                    placeholder='What needs to be done?'
-                    value={this.state.input}
-                    onChange={this.updateInput}
-                    onKeyDown={this.handleAddTodo}
-                />
-                <TodoList
-                    todos={this.state.todos}
-                    filter={this.state.filter}
-                    editKey={this.state.editKey}
-                    onDeleteTodo={this.handleDeleteTodo}
-                    onToggleTodo={this.handleToggleTodo}
-                    onChangeTodoText={this.handleChangeTodoText}
-
-                    onEditItem={this.handleEditItem}
-                    onUpdateEditText={this.updateEditText}
-                    value={this.state.editText}
-                />
-                <ListMenu
-                    todos={this.state.todos}
-                    onFilterChange={this.updateFilter}
-                    onClearCompleted={this.handleClearCompleted}
-                />
-            </React.Fragment>
-        )
-    }
 }
+
+export default Todos
